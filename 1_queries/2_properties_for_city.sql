1d0939d6d93c2464ef04aaa92ad6e2bfd32a5783
sachieko <arashikabane@gmail.com>
2022-08-24 22:10:15 -0600

    case sensitive but exact match required for this query

--- 1_queries/2_properties_for_city.sql
+++ 1_queries/2_properties_for_city.sql
@@ -1,6 +1,6 @@
 SELECT properties.id as property_id, title, cost_per_night, city, ROUND(AVG(rating),3) as average_rating FROM properties 
 JOIN property_reviews ON property_id = properties.id
-WHERE city ILIKE '%vancouver%'
+WHERE city ILIKE 'vancouver'
 GROUP BY properties.id
 HAVING ROUND(AVG(rating),3) >= 4
 ORDER BY cost_per_night LIMIT 10;
