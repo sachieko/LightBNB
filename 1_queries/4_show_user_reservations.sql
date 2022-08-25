7063f50ee60c6ce8e52531e4cabfebff5e405ce4
sachieko <arashikabane@gmail.com>
2022-08-25 13:46:47 -0600

    Change id for tests

--- 1_queries/4_show_user_reservations.sql
+++ 1_queries/4_show_user_reservations.sql
@@ -2,6 +2,6 @@ SELECT reservations.id, properties.title, reservations.start_date, properties.co
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON property_reviews.property_id = properties.id
-WHERE reservations.guest_id = 3
+WHERE reservations.guest_id = 2
 GROUP BY properties.id, reservations.id
 ORDER BY reservations.start_date;
